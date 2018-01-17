eeb6cea8aa5af8f67af3d1bfdafcef161303847c
sabareshwaran <35525961+sabareshwaran@users.noreply.github.com>
2018-01-17 16:49:55 +0530

    ODDEVEN.C
    NUMBER IS ODD OR EVEN

---
+++ ODDEVEN.C
@@ -0,0 +1,12 @@
+#INCLUDE<STDIO.H>
+VOID MAIN()
+{
+int number;
+printf("enter a number:");
+scanf("%d",&number);
+if(number %2==0)
+printf("%d is even",number);
+else
+printf("%d is odd",number);
+return 0;
+}
