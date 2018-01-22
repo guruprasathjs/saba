31d93bb171cbdbdb086c64638a5ca985a8df8b24
sabareshwaran <35525961+sabareshwaran@users.noreply.github.com>
2018-01-22 16:37:25 +0530

    Create vovelorconstant.c

---
+++ vovelorconstant.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main()
+{
+char c;
+int vovels;
+printf("enter a alphabet:\n");
+scanf("%c",&c);
+isvovels=(c=='a', c=='e' , c=='i' , c=='o' , c=='u');
+if(isvovels)
+printf("%c is a vovel:",c);
+else
+printf("%c is constants:",c);
+return 0:
+}
