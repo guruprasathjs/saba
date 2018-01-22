bfca113e13537e46e51009c89ef564c525429cd5
sabareshwaran <35525961+sabareshwaran@users.noreply.github.com>
2018-01-22 16:00:06 +0530

    pnorzero.c
    number is positive negative or zero

---
+++ pnorzero.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main()
+{
+int num;
+printf("enter a number:\n");
+scanf("%d",&num);
+if (num>0)
+printf("%d is positive\n",num);
+else
+printf("%d is negative\n",num);
+else
+printf("zero");
+}
