3be1486bc5798905640b919e12f02576fa06f4fe
guruprasathjs <35652213+guruprasathjs@users.noreply.github.com>
2018-01-23 16:43:48 +0530

    Update alphabetornot.c

--- alphabetornot.c
+++ alphabetornot.c
@@ -2,11 +2,9 @@
 int main()
 {
 char c;
-printf("enter a character:\n");
 scanf("%c",&c);
-if(c>='a' && c<'z')
+if(c>='a' && c<='z')
 printf("%c is alphabet",c);
 else
 printf("%c is not",c);
-return 0;
 }
